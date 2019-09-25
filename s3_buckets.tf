31e69523e47b27793821e1ac58f9f09eca686abf
Dincho Todorov <dincho@users.noreply.github.com>
2019-09-25 15:34:29 +0300

    Add snapshots distribution

--- s3_buckets.tf
+++
@@ -1,7 +0,0 @@
-resource "aws_s3_bucket" "aeternity-database-backups" {
-  bucket = "aeternity-database-backups"
-
-  acl           = "private"
-  force_destroy = false
-  region        = "eu-central-1"
-}
