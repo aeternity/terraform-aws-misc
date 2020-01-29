b0fac215e82a8be607d844df6452ebbfd4c0f1d8
Ivaylo Papazov <papazov@vmandco.com>
2020-01-29 17:34:01 +0200

    Wildcard certificate

--- builds.tf
+++ builds.tf
@@ -19,26 +19,6 @@ resource "aws_s3_bucket" "aeternity-node-builds" {
   }
 }
 
-resource "aws_acm_certificate" "builds" {
-  provider          = aws.us-east-1
-  domain_name       = local.builds_fqdn
-  validation_method = "DNS"
-}
-
-resource "aws_route53_record" "builds_cert_validation" {
-  zone_id = var.zone_id
-  name    = aws_acm_certificate.builds.domain_validation_options.0.resource_record_name
-  type    = aws_acm_certificate.builds.domain_validation_options.0.resource_record_type
-  records = [aws_acm_certificate.builds.domain_validation_options.0.resource_record_value]
-  ttl     = 300
-}
-
-resource "aws_acm_certificate_validation" "builds" {
-  provider                = aws.us-east-1
-  certificate_arn         = aws_acm_certificate.builds.arn
-  validation_record_fqdns = [aws_route53_record.builds_cert_validation.fqdn]
-}
-
 resource "aws_cloudfront_distribution" "builds" {
   enabled         = true
   aliases         = [local.builds_fqdn]
@@ -72,7 +52,7 @@ resource "aws_cloudfront_distribution" "builds" {
   }
 
   viewer_certificate {
-    acm_certificate_arn      = aws_acm_certificate_validation.builds.certificate_arn
+    acm_certificate_arn      = var.cert_arn_wildcard_services
     ssl_support_method       = "sni-only"
     minimum_protocol_version = "TLSv1.1_2016"
   }
