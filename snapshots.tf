c59ff5121a5666490bf72c6b3b02952a68d199fb
Ivaylo Papazov <IvayloPapazov@users.noreply.github.com>
2020-01-30 15:18:52 +0200

    Wildcard certificate (#6)

--- snapshots.tf
+++ snapshots.tf
@@ -19,26 +19,6 @@ resource "aws_s3_bucket" "aeternity-node-snapshots" {
   }
 }
 
-resource "aws_acm_certificate" "snapshots" {
-  provider          = aws.us-east-1
-  domain_name       = local.snapshots_fqdn
-  validation_method = "DNS"
-}
-
-resource "aws_route53_record" "snapshots_cert_validation" {
-  zone_id = var.zone_id
-  name    = aws_acm_certificate.snapshots.domain_validation_options.0.resource_record_name
-  type    = aws_acm_certificate.snapshots.domain_validation_options.0.resource_record_type
-  records = [aws_acm_certificate.snapshots.domain_validation_options.0.resource_record_value]
-  ttl     = 300
-}
-
-resource "aws_acm_certificate_validation" "snapshots" {
-  provider                = aws.us-east-1
-  certificate_arn         = aws_acm_certificate.snapshots.arn
-  validation_record_fqdns = [aws_route53_record.snapshots_cert_validation.fqdn]
-}
-
 resource "aws_cloudfront_distribution" "snapshots" {
   enabled         = true
   aliases         = [local.snapshots_fqdn]
@@ -72,7 +52,7 @@ resource "aws_cloudfront_distribution" "snapshots" {
   }
 
   viewer_certificate {
-    acm_certificate_arn      = aws_acm_certificate_validation.snapshots.certificate_arn
+    acm_certificate_arn      = var.cert_arn_wildcard_services
     ssl_support_method       = "sni-only"
     minimum_protocol_version = "TLSv1.1_2016"
   }
--- variables.tf
+++ variables.tf
@@ -13,3 +13,7 @@ variable "ops_zone_id" {
 variable "ops_domain" {
   default = "ops.aeternity.com"
 }
+
+variable "cert_arn_wildcard_services" {
+  default = "arn:aws:acm:us-east-1:106102538874:certificate/0731f2c3-06e5-4e1b-ab9c-135187ea3ea1"
+}
