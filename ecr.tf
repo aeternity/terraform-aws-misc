f105a20dee2539cd79376a3af0b58c330f4c45d6
Dincho Todorov <dincho@users.noreply.github.com>
2024-07-05 18:14:22 +0300

    Add public ECR repositories

---
+++ ecr.tf
@@ -0,0 +1,24 @@
+locals {
+  repositories = toset([
+    "aemon",
+    "aerepl-web",
+    "aesophia_http",
+    "mdw-frontend",
+    "aepp-base",
+    "aepp-contracts",
+    "aepp-faucet-nodejs",
+    "aepp-hyperchain",
+    "aepp-bridge",
+    "aepp-graffiti",
+    "graffiti-server",
+    "aepp-governance",
+    "governance-server",
+  ])
+}
+
+resource "aws_ecrpublic_repository" "repo" {
+  for_each        = local.repositories
+  repository_name = each.key
+
+  provider = aws.us-east-1
+}
